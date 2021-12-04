69bd04d3a9b87e300acfceb8f70e3fbbeb5d8c28
Anh Nguyen <anhknguyen97@yahoo.com>
2021-12-04 00:48:19 -0800

    Create graphql api

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!'
+        },
+        name() {
+            return 'Anh Nguyen'
+        },
+        location() {
+            return 'Bay Area'
+        },
+        bio() {
+            return 'SWE'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
